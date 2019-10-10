251ec3831da93c01a01ea7f86c39c68323ffec00
Preetha Pant <pree@hawaii.edu>
2019-10-09 18:46:52 -1000

    Create Ex.js

---
+++ Lab9/Ex.js
@@ -0,0 +1,14 @@
+date= "5/26/1999";
+step1 = 99; // year born result = 99
+
+step2 = parseInt(step1/4); // divide year by 4 & keep whole number result = 24
+step3 = step2+step1;
+step4= 1;
+step6=step4+step3;
+step7= 26+step6;
+step8=step7;
+console.log(step8%7);
+
+
+
+
