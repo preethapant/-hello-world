f9513418114dfdbc130370f171bb9d1f6dd0360b
Preetha Pant <pree@hawaii.edu>
2019-11-12 22:54:12 -1000

    lab 13
    adding lab 13

---
+++ Pant_Preetha_Assignment1/product_data.js
@@ -0,0 +1,23 @@
+Products=[
+    { "product": "Towels",  
+        "price": 10.00,  
+        "image": "towels.jpg"
+    },
+    { "product": "Swimsuit",  
+    "price": 30.00,  
+    "image": "swimsuit.jpg"
+    },
+    { "product": "Sunscreen",  
+    "price": 7.00,  
+    "image": "sunscreen.jpg"
+    },
+    { "product": "Hat",  
+    "price": 12.00,  
+    "image": "hat.jpg"
+    },
+    { "product": "Sunglasses",  
+    "price": 45.00,  
+    "image": "sunglasses.jpg"
+    }
+];
+if(typeof modules !='undefined'){exports.products=products}
