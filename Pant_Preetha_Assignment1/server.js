ea3a0bc0d9a5ccd5eaf5a99bb11bed8931f006cb
Preetha Pant <pree@hawaii.edu>
2019-11-14 15:48:40 -1000

    k

--- Pant_Preetha_Assignment1/server.js
+++ Pant_Preetha_Assignment1/server.js
@@ -6,7 +6,7 @@ var products = require('./public/product_data.js');//take data from product_data
 
 var express = require('express');// start express package to set up server
 var myParser = require("body-parser");//start body-parser to set up server
-var fs = require('fs');
+
 var app = express();//starts express
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);//respond to HTTP request by sending type of request and the path of request
@@ -17,26 +17,28 @@ app.use(myParser.urlencoded({ extended: true }));
 //checks purchase submission, if good gives invoice or else referes back to order page
 app.get("/process_form", function (request, response) {
     //checks if data is valid
-    //finds request.query
+    var params = request.query;
+    console.log(params);
     if (typeof params['purchase_submit'] != 'undefined') {
         has_errors = false; // assume quantities are valid from the start
         total_qty = 0; // need to check if something was selected so we will look if the total > 0
         for (i = 0; i < products.length; i++) {
-            if (typeofparams[`quantity${i}`] != 'undefined') {
-                a_qty = params.get[`quantity${i}`];
+            if (typeof params[`quantity${i}`] != 'undefined') {
+                a_qty = params[`quantity${i}`];
+                console.log(a_qty);
                 total_qty += a_qty;
                 if (!isNonNegInt(a_qty)) {
                     has_errors = true; // oops, invalid quantity
                 }
             }
         }
+        console.log(has_errors,total_qty);
         //request view query string
-        qstr = querystring.stringify(request.query);//
+        qstr = querystring.stringify(request.query);//turn object to query string
         // Now respond to errors or redirect to invoice if all is ok
         if (has_errors || total_qty == 0) {
             //if quantity data is not valid, send them back to product display
-            qstr = querystring.stringify(request.query);
-            response.redirect("product_page.html?" + qstr);
+            response.redirect("products_page.html?" + qstr);
         } else { // all good to go!
             response.redirect("invoice.html?" + qstr);
         }
@@ -49,6 +51,7 @@ app.listen(8080, () => console.log(`listening on port 8080`));
 //Adapted from Lab 13
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
+    if(q==""){q=0;}
     if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
     if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it is an integer
