3837c08781639d9da34940dbca9185092ea836ae
Preetha Pant <pree@hawaii.edu>
2019-12-02 12:34:20 -1000

    changes

--- Pant_Preetha_Assignment2/server.js
+++ Pant_Preetha_Assignment2/server.js
@@ -1,63 +1,21 @@
 //PREETHA PANT, 11/26/19, creating own server to serve up our website
-//Adapted from Lab 13
+const querystring = require('querystring');//require that the server responds to any errors
+var fs = require('fs');//getting the component fs and loading it in and saving it in the module fs, because when you do a require it creates a module 
 var express = require('express');// start express package to set up server
-var app = express();//starts express
 var myParser = require("body-parser");//start body-parser to set up server
 var products = require('./public/product_data.js');//take data from product_data.js in the public folder
-const querystring = require('querystring');//require that the server responds to any errors 
-var filename = 'user_data.json';// storing the user_data.json under the name filename
-var fs = require('fs');//getting the component fs and loading it in and saving it in the module fs, because when you do a require it creates a module 
+var qs = require('querystring');
 //Adapted from Lab 13
-//returning errors check in the value being added to the products
-function isNonNegInt(q, returnErrors = false) {
-    errors = []; // assume no errors at first
-    if(q==""){q=0;}//checks to see whether or not the quantity is 0
-    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it is an integer
-    return returnErrors ? errors : (errors.length == 0); //include that if no amount is added the quantity is 0
-}
-//Adapted from Lab 14
-//check to see if the file exists. if it does, read it and parse it. if not output a message
-if (fs.statSync(filename)) {
 
-    //returns contents of the path
-    data = fs.readFileSync(filename, 'utf-8');
-
-    stats = fs.statSync(filename);
-    console.log(filename + ' has ' + stats.size + ' characters');//logging the amount of characters in each file on the console
-    users_reg_data = JSON.parse(data);//parses the data into JSON format
-    /*
-    username = 'newuser';
-    users_reg_data[username] = {}; //new user becomes new property of users_reg_data object
-    users_reg_data[username].password = 'newpass';
-    users_reg_data[username].email = 'newuser@user.com';
-    fs.writeFileSync(filename, JSON.stringify(users_reg_data));
-    */
-
-    console.log(users_reg_data); //console logs
-
-} else {
-    console.log(filename + ' does not exist');
-}
-
-//Adapted from Lab 14
-app.use(myParser.urlencoded({ extended: true }));
-
-var filename = 'user_data.json';// storing the user_data.json under the name filename
-
-//Adapted from Lab 13
-//loging the method and path displayed in the console
-
+var app = express();//starts express
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);//respond to HTTP request by sending type of request and the path of request
     next();//calls the middleware function
 });
-
-//Adapted from Lab 13
-//posts the process form data with the action as process_form
+var user_product_quantities = {};
 app.get("/process_form", function (request, response) {
-    //checks if quantity is valid 
+    //checks if quantity is valid
+    user_product_quantities = request.query; 
     //looks up a request.query
     var params = request.query;
     console.log(params);
@@ -74,109 +32,220 @@ app.get("/process_form", function (request, response) {
                 }
             }
         }
-        console.log(has_errors,total_qty);
+        console.log(has_errors, total_qty);
         //request to view query string
         qstr = querystring.stringify(request.query);//turn object to query string
         // Now respond to errors or redirect to invoice if all is ok
         if (has_errors || total_qty == 0) {//check errors in total quantity
             response.redirect("products_page.html?" + qstr);//direct the page to products_page if errors in data entry
-        } else { 
+        } else {
             response.redirect("login.html?" + qstr);// direct the page to invoice if no errors in data entry
         }
     }
 });
-app.post("/process_form", function (request, response) {
-    let POST = request.body;
-    var hasValidQuantities = true; //action taken given that the quantities textbox is true
-    var hasPurchases = false; //action taken given the quantity of purchases are false
-    for (i = 0; i < products.length; i++) { //for loop for each product in the array that increases the count by 1
-        q = POST['quantity' + i]; //quantity entered by the user for a product(s) is assigned into q
-        if (isNonNegInt(q) == false) { //if the quantity enteredby the user is not a valid integer
-            hasValidQuantities = false; //HasValidQuantities is false or nothing was purchased
-        }
-        if (q > 0) { //if the quantity entered is more than 0
-            hasPurchases = true; //hasPurchases is true and the user has entered a valid integer(s) to purchase something
-        }
-    }
-    //if data is valid give user an invoice, if not give them an error
-    var qString = querystring.stringify(POST); //string query together
-    if (hasValidQuantities == true && hasPurchases == true) {//if hasValidQuantities and hasPurchases is true
-        response.redirect('login.html?' + qString);
-    }
-    else {
-
-        response.redirect("./products_page.html?" + qString); //if quantity is not valid, user is sent back to product page along with the query data entered previously from the user
-    }
-
-});
+//checking that data is valid
+//borrowed code from Lab13/Assigment1
+function isNonNegInt(q, returnErrors = false) {
+    errors = []; // assume no errors at first
+    if (q == "") { q = 0; }//checks to see whether or not the quantity is 0
+    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if (parseInt(q) != q) errors.push(' Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : (errors.length == 0); //include that if no amount is added the quantity is 0
+}
 
 
+//Adapted from Lab 14
+//check to see if the file exists. if it does, read it and parse it. if not output a message
+app.use(myParser.urlencoded({ extended: true }));
 
-//Got from Lab14
-//gets called with data from the form
-app.post("/login", function (request, response) {
-    // Process login form POST and redirect to logged in page if ok, back to login page if not
-    console.log(request.body);
-    var qString = querystring.stringify(request.query);
-    inputUser = request.body.username;
-    inputPass = request.body.password;
-    the_username = request.body.username.toLowerCase(); //give me username from object and assigning it
-    if (typeof users_reg_data[the_username] != 'undefined') { //ask object if it has property called username, if it does, it wont be udefined. check to see if it exists
-        if (users_reg_data[the_username].password == request.body.password) {//check if the password they entered matches what was stored
-            //passes the username + the string logged in on the page to greet them
-           
-            //request.query.InvoiceName = users_reg_data[the_username].name;
-            presentFullName = users_reg_data[the_username].name;
-            stickPut = presentFullName;
-            request.query.stickMe2 = stickPut; 
-            qString = querystring.stringify(request.query);
-            response.redirect("./invoice.html?" + qString);//if the quantity is valid, user is directed to invoice along with the query data from the form
-            
-        } else if (users_reg_data[the_username].password != request.body.password) {
-            error = "Incorrect Password";
-            stickInput = inputUser;//if they did not login successfully, does another get request and redirects user to login to page
-            
-        }
-    } else {
-        error = the_username + "is not registered";
-        stickInput = inputUser;
-    }
-    request.query.LoginError = error;
-    request.query.stickies = stickInput;
-    qString = querystring.stringify(request.query);
-    response.redirect("./login.html?" + qString);
+var filename = 'user_data.json';// storing the user_data.json under the name filename
 
-
-
-});  
-  
-
-app.post("/register", function (request, response) {
-    // process a simple register form
-
-    //validate registration data
-
-    //all good, so save new user to the file name(registration data)
+if (fs.statSync(filename)) {// load in users_reg_data from the json file
+stats = fs.statSync(filename);
+    
+    console.log(filename); //returns user_data
+    data = fs.readFileSync(filename, 'utf-8');//read the file synchronously until the file comes back
+    users_reg_data = JSON.parse(data);//parses the data into JSON format
+    console.log(users_reg_data); //console log output
+    /*
     username = 'newuser';
-    username = request.body.username;
+    users_reg_data[username] = {}; //new user becomes new property of users_reg_data object
+    users_reg_data[username].password = 'newpass';
+    users_reg_data[username].email = 'newuser@user.com';
+    fs.writeFileSync(filename, JSON.stringify(users_reg_data));
+    */
 
-    if (typeof users_reg_data[username] != 'undefined') {//check if the password they entered matches what was stored
-        response.redirect('./login.html?' + qString);//if they did not login successfully, does another get request and redirects user to login to page
-        //can regnerate form here and display errors
-    } else if (request.body.repeat_password != request.body.password) {
-        response.send(`password does not match`);
-    } else {
-        users_reg_data[username] = {}; //new user becomes new property of users_reg_data object
-        users_reg_data[username].password = request.body.password;
-        users_reg_data[username].email = request.body.email;
-        fs.writeFileSync(filename, JSON.stringify(users_reg_data));
-        //alert(`${username} registered!`)
-        response.redirect("./invoice.html?" + qString);
-        //response.redirect ("./invoice.html?" + qString );//if the quantity is valid, user is directed to invoice along with the query data from the form
+    
 
-        console.log(request.body);
+} else {
+    console.log(filename + ' does not exist');
+    //if file name does not exist, return this
+   //ex:if we change var filename = 'user_data.json'; will return user_data.jsondoes not exist!
+}
+app.post("/login.html", function (request, response) {
+    //process login form POST and redirect to logged in page if ok, back to login page if not
+    //if I have post, below will load
+    console.log(user_product_quantities);
+    //the_username =users_reg_data.username;
+    the_username = request.body.username;
+    console.log( "Username is", the_username);
+    console.log(users_reg_data);
+    //validate login data
+    if (typeof users_reg_data[the_username] != 'undefined') { //data we loaded in the file
+       if (users_reg_data[the_username].password == request.body.password) {
+          //make the query string of product quantity needed for invoice
+          theQuantQuerystring = qs.stringify(user_product_quantities);
+          response.redirect('invoice.html?' + theQuantQuerystring + `&username=${the_username}`);
+          //add their username in the invoice so that they know they're logged in (for personalization)
+       } else {
+          response.redirect('login.html'); //if username doesn't exist then return to login page 
+          //make username sticky (i.e., stay in page when redirected)
+          //NEED TO ADD MESSAGE ABOUT IF USERNAME AND PASSWORD ARE INCORRECT
+       }
     }
+ });
+ 
+ app.post("/register.html", function (request, response) {
+    let INFO = request.body;
+    //Makes the username case-insensitive
+    username = INFO.Username.toLowerCase();
+    //Resest Errors in string so them dont carry over if user messes up multiple 
+    //process a simple register form
+ 
+    //validate registration data (add validation code for Assignment2)
+    
+    // SELECTING ALL TEXT ELEMENTS
+var username = document.forms['vform']['username'];
+var email = document.forms['vform']['email'];
+var password = document.forms['vform']['password'];
+var password_confirm = document.forms['vform']['password_confirm'];
+// SELECTING ALL ERROR DISPLAY ELEMENTS
+var name_error = document.getElementById('name_error');
+var email_error = document.getElementById('email_error');
+var password_error = document.getElementById('password_error');
+// SETTING ALL EVENT LISTENERS
+username.addEventListener('blur', nameVerify, true);
+email.addEventListener('blur', emailVerify, true);
+password.addEventListener('blur', passwordVerify, true);
+// validation function
+function Validate() {
+  // validate username
+  if (username.value == "") {
+    username.style.border = "1px solid red";
+    document.getElementById('username_div').style.color = "red";
+    name_error.textContent = "Username is required";
+    username.focus();
+    return false;
+  }
+  // validate username
+  if (username.value.length < 3) {
+    username.style.border = "1px solid red";
+    document.getElementById('username_div').style.color = "red";
+    name_error.textContent = "Username must be at least 3 characters";
+    username.focus();
+    return false;
+  }
+  // validate email
+  if (email.value == "") {
+    email.style.border = "1px solid red";
+    document.getElementById('email_div').style.color = "red";
+    email_error.textContent = "Email is required";
+    email.focus();
+    return false;
+  }
+  // validate password
+  if (password.value == "") {
+    password.style.border = "1px solid red";
+    document.getElementById('password_div').style.color = "red";
+    password_confirm.style.border = "1px solid red";
+    password_error.textContent = "Password is required";
+    password.focus();
+    return false;
+  }
+  // check if the two passwords match
+  if (password.value != password_confirm.value) {
+    password.style.border = "1px solid red";
+    document.getElementById('pass_confirm_div').style.color = "red";
+    password_confirm.style.border = "1px solid red";
+    password_error.innerHTML = "The two passwords do not match";
+    return false;
+  }
+}
+// event handler functions
+function nameVerify() {
+  if (username.value != "") {
+   username.style.border = "1px solid #5e6e66";
+   document.getElementById('username_div').style.color = "#5e6e66";
+   name_error.innerHTML = "";
+   return true;
+  }
+}
+function emailVerify() {
+  if (email.value != "") {
+  	email.style.border = "1px solid #5e6e66";
+  	document.getElementById('email_div').style.color = "#5e6e66";
+  	email_error.innerHTML = "";
+  	return true;
+  }
+}
+function passwordVerify() {
+  if (password.value != "") {
+  	password.style.border = "1px solid #5e6e66";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	document.getElementById('password_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+  if (password.value === password_confirm.value) {
+  	password.style.border = "1px solid #5e6e66";
+  	document.getElementById('pass_confirm_div').style.color = "#5e6e66";
+  	password_error.innerHTML = "";
+  	return true;
+  }
+}
+    //validation includes # of characters, capitalization of letters, confirm password by typing it a second time (repeat_password)
+    has_errors = false;
+    //if all good, so save the new user, then redirect to invoice otherwise bounce back to register
+ 
+    //do validation
+    
+    //need to validate username length + case sentsitivity
+    //need to validate pass word lenght + one capital letter minimum + numbers  
+       //make sure it matches what's in register.html
+       //if can't figure out something, can always take it out of required field for password in register.html
+    //need to validate passord_cofirmation is the same as password
+    //need to validate email?
+ 
+ 
+    if (has_errors == false) {
+       // NEED TO save registration data to file
+        //creates new user
+        username = request.body.username;
+        users_reg_data[username] = {};
+        users_reg_data[username].password = request.body.password;
+        users_reg_data[username].name = request.body.name;
+        users_reg_data[username].email = request.body.email;
+     
+        //turns into a json string file
+        fs.writeFileSync(filename, JSON.stringify(users_reg_data));
+       //I think he said I can use what's below but just changeg data in the pharanthesis ot something else but I forgot...
+       //then change fs.readFileSync to fs.writeFileSync
+       
+       /*
+       data = fs.readFileSync(filename, 'utf-8'); //read the file synchronously until the file comes back
+       users_reg_data = JSON.parse(data) //will convert data (string) into an object or an array
+       */
+    
+       //make the query string of product quantity needed for invoice
+       theQuantQuerystring = qs.stringify(user_product_quantities);
+       response.redirect('invoice.html?' + theQuantQuerystring + `&username=${username}`);
+       //add their username in the invoice so that they know they're logged in (for personalization)
+    } else {
+       response.redirect('register.html'); //if username doesn't exist then return to registration page
+       //NEED TO ADD MESSAGE ABOUT IF USERNAME AND PASSWORD ARE INCORRECT
+    }
+ });
 
-});
 app.use(express.static('./public')); //retrieves get request and look for file in public directory
 app.listen(8080, () => console.log(`listening on port 8080`)); //the server listens on port 8080 and prints the message into the console
